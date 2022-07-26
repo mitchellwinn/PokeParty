30642de3b0ab5d3286514c1c1e2fe37141fa53ea
mtchwin <93198494+mtchwin@users.noreply.github.com>
2022-07-26 12:21:53 -0700

    Update client.py

--- client.py
+++ client.py
@@ -18,34 +18,33 @@ class SimpleData(object):
 
 class Client(object):
 
-    def getServerMessage(self):
-        print("Waiting for message from server...")
-            msg = self.client.recv(self.header)
-            if msg.purpose == self.DISCONNECT_MESSAGE:
-                game.gameObjects.clear()
-                game.gameState = "title"
-                del(self)
-            elif msg.purpose == "SETROOM":
-                self.room = msg.strings[0]
-                print("updated room!")
-            elif msg[0].purpose == "GETUPDATES":
-                #we don't need to do anything with an update about ourselves, as thats information we originally gave out, and this is client authoritative since its a boardgame
-                for i in game.otherPlayers:
-                    i.removeFromClient = True
-                    for thisMsg in msg:
-                        if thisMsg.id == self.id:
-                            continue
-                        referenceExists = False
-                        if i.id == thisMsg.id:
-                            referenceExists = True
-                            i.removeFromClient = False
-                            i = thisMsg
-                        elif referenceExists == False:
-                            game.otherPlayers.append(msg)
-                    if(i.removeFromClient):
-                        game.otherPlayers.remove(i)
-                game.allPlayersInRoom = game.otherPlayers
-                game.allPlayersInRoom.append(game.playerObject)
+    def serverMsgInterpret(self):
+        msg = reply
+        if msg.purpose == self.DISCONNECT_MESSAGE:
+            game.gameObjects.clear()
+            game.gameState = "title"
+            del(self)
+        elif msg.purpose == "SETROOM":
+            self.room = msg.strings[0]
+            print("updated room!")
+        elif msg[0].purpose == "GETUPDATES":
+            #we don't need to do anything with an update about ourselves, as thats information we originally gave out, and this is client authoritative since its a boardgame
+            for i in game.otherPlayers:
+                i.removeFromClient = True
+                for thisMsg in msg:
+                    if thisMsg.id == self.id:
+                        continue
+                    referenceExists = False
+                    if i.id == thisMsg.id:
+                        referenceExists = True
+                        i.removeFromClient = False
+                        i = thisMsg
+                    elif referenceExists == False:
+                        game.otherPlayers.append(msg)
+                if(i.removeFromClient):
+                    game.otherPlayers.remove(i)
+            game.allPlayersInRoom = game.otherPlayers
+            game.allPlayersInRoom.append(game.playerObject)
 
     def send(self, data):
         try:
@@ -92,4 +91,3 @@ class Client(object):
 
         #self.serverHandler()
         self.send (SimpleData("ROOM",[self.desiredRoom,self.id]).getAsDataString())
-        self.getServerMessage()
